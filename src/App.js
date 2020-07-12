e4a15999b45a5d9f6c72a6f5610f376852466936
Jeongmin Park <wjd326als@naver.com>
2020-07-12 20:51:32 +0900

    fin

--- src/App.js
+++ src/App.js
@@ -1,52 +1,19 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get(
-      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
-    );
-    this.setState({ movies, isLoading: false });
-  };
-  async componentDidMount() {
-    this.getMovies();
-  }
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section class="container">
-        {isLoading ? (
-          <div class="loader">
-            <span class="loader__text"></span>
-          </div>
-        ) : (
-          <div class="movies">
-            {movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail} />
+    </HashRouter>
+  );
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,26 +3,26 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({ id, year, title, summary, poster, genres }) {
+function Movie({ year, title, summary, poster, genres }) {
   return (
-    <div className="movie">
-      <Link
-        to={{
-          pathname: `/movie/${id}`,
-          state: {
-            year,
-            title,
-            summary,
-            poster,
-            genres,
-          },
-        }}
-      >
+    <Link
+      to={{
+        pathname: "/movie-detail", //props 전송
+        state: {
+          year,
+          title,
+          summary,
+          poster,
+          genres,
+        },
+      }}
+    >
+      <div className="movie">
         <img src={poster} alt={title} title={title} />
         <div className="movie__data">
           <h3 className="movie__title">{title}</h3>
           <h5 className="movie__year">{year}</h5>
-          <ul className="movie__genres">
+          <ul className="genres">
             {genres.map((genre, index) => (
               <li key={index} className="genres__genre">
                 {genre}
@@ -31,8 +31,8 @@ function Movie({ id, year, title, summary, poster, genres }) {
           </ul>
           <p className="movie__summary">{summary.slice(0, 180)}...</p>
         </div>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 }
 
