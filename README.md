00fb3e98b74fdc7691fd70d66116a3d02cdc4ac3
jeongmin <wjd326als@naver.com>
2020-07-15 00:43:42 +0900

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# Movie App
-
-React JS Fundamentals Coure
---
+++ precache-manifest.6604d4e0afb60a3ab98bbdb9eb56bba1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "230e12ac81f35acc9fd4454e73dc97a4",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "f2019dbd0cde33be24d9",
+    "url": "/movie_app_2019/static/css/main.f0594005.chunk.css"
+  },
+  {
+    "revision": "b52157e597c6acbc702d",
+    "url": "/movie_app_2019/static/js/2.fe2d4bf0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2019/static/js/2.fe2d4bf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2019dbd0cde33be24d9",
+    "url": "/movie_app_2019/static/js/main.838cfc7c.chunk.js"
+  },
+  {
+    "revision": "10a8e46fe0d1ddf64ca9",
+    "url": "/movie_app_2019/static/js/runtime-main.15105636.js"
+  }
+]);
